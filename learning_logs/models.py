7606e40eb66ad289c1679e1887393e631c1ffc7f
jwatts82 <jessperr182@hotmail.com>
2021-03-07 20:25:39 -0800

    modify database

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -6,6 +6,6 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
-    def __str_-(self):
+    def __str__(self):
         """Return a string representation fo the model."""
         return self.text
